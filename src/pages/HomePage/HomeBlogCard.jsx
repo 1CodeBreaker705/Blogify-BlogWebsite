5893722732a935b280c7cc3fc3ae176cd31cfb4b
1CodeBreaker705 <ranjansingh0661@gmail.com>
2025-12-16 22:41:51 +0530

    Update HomeBlogCard.jsx

--- src/pages/HomePage/HomeBlogCard.jsx
+++ src/pages/HomePage/HomeBlogCard.jsx
@@ -10,10 +10,12 @@ const HomeBlogCard = ({data}) => {
   const DEFAULT_AVATAR = '/defaultAvatar.svg'
 
   const [user,setUser]=useState({})
-  const [images,setImages]=useState({
-    image:'',
-    profileImage:''
-  })
+  const [profileImage, setProfileImage] = useState(DEFAULT_AVATAR)
+
+  const blogImage = `${appwriteStorage.getFileView(
+  ENVObj.VITE_APPWRITE_STORAGE_ID,
+  data.image
+)}&v=${data.$id}`;
 
   const tags=data?.tags?.split(",")
 
@@ -21,9 +23,8 @@ const HomeBlogCard = ({data}) => {
      try {
        const {documents} = await appwriteDatabase.listDocuments(ENVObj.VITE_APPWRITE_DB_ID,ENVObj.VITE_APPWRITE_PROFILE_COLLECTION_ID,[Query.equal('user',data.user),Query.select(['name','image'])])
        setUser(documents[0])
-            const profileImage = documents[0].image === 'defaultAvatar.svg' || !documents[0].image? DEFAULT_AVATAR: appwriteStorage.getFileView(ENVObj.VITE_APPWRITE_STORAGE_ID, documents[0].image)
-        const image=appwriteStorage.getFileView(ENVObj.VITE_APPWRITE_STORAGE_ID,data.image)
-        setImages({image,profileImage})
+       const profileImage = documents[0].image === 'defaultAvatar.svg' || !documents[0].image ? DEFAULT_AVATAR : `${appwriteStorage.getFileView(ENVObj.VITE_APPWRITE_STORAGE_ID,documents[0].image)}&v=${documents[0].$id}`;
+       setProfileImage(profileImage)
      } catch (error) {
        toast.error(error.message)
      }
@@ -31,7 +32,7 @@ const HomeBlogCard = ({data}) => {
 
   useEffect(()=>{
     fetchUserDetail()
-  },[])
+  },[data.user]])
 
   
   
@@ -41,7 +42,7 @@ const HomeBlogCard = ({data}) => {
 <Link to={`/blog/${data.slug}`} className='lg:p-4 my-1'>
   <div className="bg-section border-zinc-800  border hover:border-pink-500/40  hover:scale-[101%] transition-all duration-300 w-full p-3 rounded-lg shadow-md shadow-black flex flex-col h-[500px] lg:h-full">
       <div className='h-40 lg:h-56 overflow-hidden'>
-        <img className=" w-full h-full rounded-md hover:scale-105 transition-all duration-300 " src={images.image} alt='' />
+        <img className=" w-full h-full rounded-md hover:scale-105 transition-all duration-300 " src={blogImage} alt='' />
       </div>
         <h5 className="mt-6 mb-2 text-xl lg:text-2xl font-psemibold tracking-tight text-heading">{data.title?.substring(0,100)}{data.title?.length>100?'...':''}</h5>
         <p className="mb-4 text-zinc-300 leading-normal font-pregular">{data.description?.substring(0,100)}{data.description?.length>100?'...':''}</p>
@@ -56,7 +57,7 @@ const HomeBlogCard = ({data}) => {
             )}
         </ul>
         <div className='flex items-center gap-x-2 mt-auto'>
-          {images.profileImage && <img src={images.profileImage} alt={user.name} className='w-10 h-10 rounded-full '  onError={(e) => (e.currentTarget.src = DEFAULT_AVATAR)} />}
+          {images.profileImage && <img src={profileImage} alt={user.name} className='w-10 h-10 rounded-full '  onError={(e) => (e.currentTarget.src = DEFAULT_AVATAR)} />}
           <div className='flex flex-col'>
             <p className='text-zinc-200 font-semibold' >{user.name}</p>
             <p className="text-end text-xs text-zinc-300">{moment(data.$createdAt).format("LL")}</p>
