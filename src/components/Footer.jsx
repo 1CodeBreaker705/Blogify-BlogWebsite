a3464109b545761f078cb20c4e177f2ccc8b2405
1CodeBreaker705 <ranjansingh0661@gmail.com>
2026-05-30 07:51:15 +0530

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,7 +4,7 @@ import Logo from './Logo'
 const Footer = () => {
   return (
     <div className='py-5 bg-section mt-15'>
-      <p className="text-center">Copyright<span className='text-btn font-pbold'>@</span><span>{new Date().getFullYear()}</span></p>
+      <p className="text-center"><span className='text-btn font-pbold'>@</span><span>{new Date().getFullYear()}</span> Blogify. All rights reserved.</p>
     </div>
   )
 }
